550895e67328539095234537f527f205a09e6032
mccabete <tmccabe@umd.edu>
2025-06-24 19:45:27 +0000

    standardize inputs, 2/3

--- utility_functions/fwiVis/fwiVis.py
+++ utility_functions/fwiVis/fwiVis.py
@@ -20,6 +20,7 @@ import folium
 from folium import plugins
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
 import datetime
+from datetime import timedelta
 import requests
 from bs4 import BeautifulSoup
 
@@ -1278,3 +1279,122 @@ def get_gridded_fwi(fireID):
         return(grid_FWI)
     else:
         return(None)
+
+def chop_fires_at_end(df):
+    '''
+    Function to extract FWI values from nccs. Returns a DF of FWI and FWI with a lead time of 8 days. 
+    
+    INPUTS:
+    
+        df (DataFrame) Single-fireID df that will trim excess observations after the final fire growth increment. TO be used with groupby fireID.    
+    '''
+    if(len(df[df.n_newpixels > 0]) > 0 ): 
+        final_t = df[df.n_newpixels > 0].t.max()
+        df = df[df.t <= final_t]
+    else:
+        df = df[df.t == df.t.min()] ## If the fire had it's only growth increment during the 12 hour period we aren't looking at (ie spread in the AM overpass)
+    return(df)
+    
+def just_the_igs(df,): ## Reminder that this will only acocunt for the earliest ignition from a multi-fire complex. 
+
+    df["is_ig"] = False
+    df.loc[df.t == df.t.min(), ['is_ig']] = True
+    
+    return(df)
+
+def extinction(df, days_past = 1):
+    df.loc[:, ['is_ext']] = False
+    df.loc[:, ["t"]] = df.t.astype("datetime64[ns]")
+    final_t = df[df.n_newpixels > 0].t.astype("datetime64[ns]").max()
+    post_fire = final_t + timedelta(days = days_past)
+
+    if (pd.isnull(np.datetime64(str(final_t)))):
+        df.loc[(df.t == df.t.min()), ['is_ext']] = True
+    else:
+        df.loc[(df.t  > final_t)  & (df.t  <= post_fire), ['is_ext']] = True
+    #print(final_t)
+    #print(df.fireID.unique())
+    #print(len(df[df["is_ext"] == True]))
+    return(df)
+
+def formatting_read_in(path, start_time = '2023-05-31 00:00:00', end_time = '2023-09-15 12:00:00', gen_cols = True, merge_with_ciffc = True): ## Does some of the read-in functions
+    '''
+    Function to standardize the read-in process of FEDS data.
+    
+    INPUTS:
+    
+        path (str): A path to a csv dataframe with FEDS fire objects and matching FWI. 
+        start_time (str): Start of fire season
+        end_time (str): end of fire season
+        gen_cols (bool): Should formatting read-in automatically generate Fire growth analysis columns? 
+        merge_with_ciffc (bool): Should function do a spatial join with CIFFC ignition data? 
+    '''
+    ### Read in
+    path = os.path.abspath(path)
+    fire3 = prep_fire_files(path)
+
+    ## Cut out end of fires
+    fire3 = fire3.sort_values(by = ["fireID", "t"])
+    fire3 = fire3.groupby("fireID").apply(chop_fires_at_end).reset_index(drop = True)
+    fire3 = fire3.drop_duplicates()
+
+    ## only keep noon overpass times
+    fire3 = fire3[~fire3.FWI.isna()]
+
+    ## subset by start and end time
+    fire3 = fire3[fire3.t >= start_time]
+    fire3 = fire3[fire3.t <= end_time]
+
+    ## exclude small fires under 4 km^2
+    # ids = fire3.groupby("fireID").farea.max().reset_index()
+    # small_fires = ids[ids.farea < 4]
+    # fire3 = fire3[~fire3.fireID.isin(small_fires)]
+
+    ## Subset to the province of quebec
+    prov = ca_prov()
+    prov = prov[prov.prov_name_en == "Quebec"]
+    prov = prov.to_crs('EPSG:3571')
+    fire3 = fire3.to_crs('EPSG:3571')
+    fire3 = fire3.sjoin(prov)
+    fire3 = fire3.drop(['prov_name_fr', 'prov_name_en', 'index_right'], axis = 1)
+    fire3 = fire3[~fire3.geometry.isna()]
+
+    if(merge_with_ciffc != gen_cols):
+        print("WARNING: Intersecting with CIFFC will subset fires, but will make the generation of some column variables different after the fact. It is recommended to generate variable columns BEFORE CIFFC spatial joins")
+
+    if(gen_cols):
+        fire3["farea_diff_stand"] = fire3.groupby("fireID").farea.diff()
+        fire3["corrected_flinelen"] = fire3.flinelen + 0.001
+        ## Fireline 
+        fire3["frac_change"] = fire3.groupby("fireID").farea.pct_change()
+        fire3["spread_bool"] = fire3.frac_change > 0
+        fire3["spread_bool"] = fire3["spread_bool"].astype("int")
+        fire3["spread_bool_frac_05"] = fire3.frac_change > 0.05
+        fire3["spread_bool_frac_05"] = fire3["spread_bool_frac_05"].astype("int")
+        fire3["spread_bool_100"] = fire3.farea_diff_stand > 1
+        fire3["spread_bool_100"] = fire3["spread_bool_100"].astype("int")
+        fire3.loc[:, "GEOS5_IMERGEARLY"] = fire3['GEOS-5.IMERGEARLY']
+        fire3 = fire3.groupby("fireID").apply(extinction).reset_index(drop = True)
+        fire3 = fire3.groupby("fireID").apply(just_the_igs).reset_index(drop = True)
+
+
+
+    ## subset to the ciffc data
+    if(merge_with_ciffc):
+        num_fires_prev = len(fire3.fireID.unique())
+        ciffc = pd.read_csv(os.path.abspath("contextual_data/CIFFC_data/ciffc_all_canada.csv"))
+        ciffc = ciffc[ciffc.field_agency_code == "qc"]
+        ciffc = gpd.GeoDataFrame(ciffc, geometry= gpd.points_from_xy(ciffc.field_longitude, ciffc.field_latitude), crs = "4326")
+        ciffc = ciffc.to_crs("3571")
+        ciffc["geometry_point"] = ciffc.geometry
+        fire3 = fire3.sjoin(ciffc)
+        num_fires_now = len(fire3.fireID.unique())
+        print(f"fire3 df had {num_fires_prev} fires p, and { num_fires_now} post CIFFC ignition join")
+
+    ### Final sorting 
+    fire3.t = fire3.t.astype("datetime64[ns]")
+    fire3 = fire3.sort_values(by = ["fireID", "t"])
+    
+    return(fire3)
+
+
